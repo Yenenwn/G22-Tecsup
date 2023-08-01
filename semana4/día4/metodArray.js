77e00e2c1fee4c8b5c17019b0edcb7ba96fde5f4
Yenenwn <josuealaniazuniga@gmail.com>
2023-07-31 20:14:04 -0500

    holaaa

---
+++ semana4/día4/metodArray.js
@@ -0,0 +1,24 @@
+//metodos arrays
+// push= agrega un elemento al final del array
+const numeros = [1,2,3,4,5,6,7]
+numeros.push(8);
+console.log(numeros)
+
+// pop = elimina  el ultimo elemento del array
+
+//s
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
